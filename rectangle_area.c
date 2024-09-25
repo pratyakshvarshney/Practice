472ec6a0b73d7ab4459939e55dab680ed25f06c8
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-25 17:54:54 +0530

    Create rectangle_area.c

---
+++ rectangle_area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //perimeter of rectangle
+    int a,b;
+    printf("enter length a of rectangle \n");
+    scanf("%d",&a);
+    printf("enter breadth b of rectangle \n");
+    scanf("%d",&b);
+    printf("area of rectangle is: %d",a*b);
+    return 0;
+}
