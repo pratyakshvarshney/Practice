044d1c22c0ee5873b82e64288f111066f14b451d
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:41:13 +0530

    Update shraddha_didi_hw.c

--- shraddha_didi_hw.c
+++ shraddha_didi_hw.c
@@ -1,4 +1,4 @@
-//Armstrong number
+//check if it is Armstrong number
 #include<stdio.h>
 #include<math.h>
 int main(){
@@ -29,3 +29,18 @@ return 0;
 }
 
 //average of three numbers
+#include<stdio.h>
+int main(){
+    int a,b,c,avg;
+    printf("enter first number \n");
+    scanf("%d",&a);
+    printf("enter second number \n");
+    scanf("%d",&b);
+    printf("enter third number \n");
+    scanf("%d",&c);
+    avg=(a+b+c)/3;
+    printf("average of %d,%d,%d is:%d",a,b,c,avg);
+    return 0;
+}
+
+//check if input character is digit or not
