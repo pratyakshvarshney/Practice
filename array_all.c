f33fed3e08417df580eebe6ac2e1266fd3245c79
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 16:59:32 +0530

    Update array_all.c

--- array_all.c
+++ array_all.c
@@ -1,17 +1,18 @@
+
 #include<stdio.h>
 
 void entry(int *a,int c){
     int i;
-    for(i=1;i<=c;i++){
-        printf("enter %d element of array \n",i);
+    for(i=0;i<c;i++){
+        printf("enter %d element of array \n",i+1);
         scanf("%d",&a[i]);
     }
 }
 
 void display(int *a, int c){
     int i ;
-    for(i=1;i<=c;i++){
-        printf("element %d of array is: %d\n",i,a[i]);
+    for(i=0;i<c;i++){
+        printf("element %d of array is: %d\n",i+1,a[i]);
     }
 }
 
@@ -30,6 +31,32 @@ void delete(int *a,int loc,int c){
     a[c-1]=0;
 }
 
+void reverse(int *a,int c){
+    int i,temp;
+    for(i=0;i<c/2;i++){
+        temp=a[i];
+        a[i]=a[(c-1)-i];
+        a[(c-1)-i]=temp;
+    }
+}
+
+void bubblesort(int *a,int c){
+    int i,j,temp,flag;
+    for(i=0;i<c;i++){
+        for(j=0;j<(c-1)-i;j++){
+            flag=0;
+            if(a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                flag=1;
+            }
+        }
+    if(flag==0)
+        break;
+    }
+}
+
 int main(){
     int c;
     printf("enter the size of array \n");
@@ -38,17 +65,25 @@ int main(){
     entry(a,c);
     display(a,c);
     
-    /*int item,loc;
+    int item,loc;
     printf("enter item, loc want to inserted \n");
     scanf("%d",&item);
     scanf("%d",&loc);
     insert(a,loc,item,c);
-    display(a,c);*/
+    display(a,c);
     
     int loc1;
     printf("enter the location from where element need to be deleted \n");
     scanf("%d",&loc1);
     delete(a,loc1,c);
+    display(a,c-1);
+    
+    printf("reversed list is : \n");
+    reverse(a,c);
+    display(a,c);
+    
+    printf("sorted array is : \n");
+    bubblesort(a,c);
     display(a,c);
     
     return 0;
