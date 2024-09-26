59c59f348acdd0b3c20752bbdab65645264df6a4
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:45:38 +0530

    Update shraddha_didi_hw.c

--- shraddha_didi_hw.c
+++ shraddha_didi_hw.c
@@ -83,3 +83,18 @@ int main() {
 }
 
 //smaller of teo numbers
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number: \n");
+    scanf("%d",&a);
+    printf("enter second number: \n");
+    scanf("%d",&b);
+    if(a<b){
+        printf("%d is smaller of two numbers provided\n",a);
+    }
+    else{
+        printf("%d is smaller of two numbers provided\n",b);
+    }
+    return 0;
+}
