57f5ef229190575fdd412cd53c3c3b958f252975
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:37:26 +0530

    Update array_all.c

--- array_all.c
+++ array_all.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 
 void entry(int *a,int c){
@@ -57,7 +56,44 @@ void bubblesort(int *a,int c){
     }
 }
 
+void linearsearch(int *a,int c,int item){
+    int i,check=0;
+    for(i=0;i<c;i++){
+        if(a[i]==item){
+            printf("search is successful , element found at %d position\n",i+1);
+            check++;
+        }
+    }
+    if(check==0){
+        printf("element not found! \n");
+    }
+}
+
+void binarysearch(int *a,int c,int item){
+    bubblesort(a,c);
+    int lb,ub,mid,i,check=0;
+    lb=0;
+    ub=c-1;
+    while(lb<=ub){
+        mid=(lb+ub)/2;
+        if(a[mid]==item){
+            printf("element found at location %d \n",mid+1);
+            check++;
+            break;
+        }
+        else if(a[mid]>item){
+            ub=mid-1;
+        }
+        else{
+            lb=mid+1;
+        }    
+    }
+    if(check==0)
+        printf("search was unsuccessful! \n");
+}
+
 int main(){
+    //for list formation and print it
     int c;
     printf("enter the size of array \n");
     scanf("%d",&c);
@@ -65,6 +101,7 @@ int main(){
     entry(a,c);
     display(a,c);
     
+    //for element insertion when location and element is given
     int item,loc;
     printf("enter item, loc want to inserted \n");
     scanf("%d",&item);
@@ -72,19 +109,31 @@ int main(){
     insert(a,loc,item,c);
     display(a,c);
     
+    //for element to be deletedwhen location is given
     int loc1;
     printf("enter the location from where element need to be deleted \n");
     scanf("%d",&loc1);
     delete(a,loc1,c);
     display(a,c-1);
     
+    //reverse the list
     printf("reversed list is : \n");
     reverse(a,c);
     display(a,c);
     
+    //sort the list
     printf("sorted array is : \n");
     bubblesort(a,c);
     display(a,c);
     
+    //search an item in list
+    int item;
+    printf("enter element to be searched: \n");
+    scanf("%d",&item);
+    //linear search
+    linearsearch(a,c,item);
+    //binary search
+    binarysearch(a,c,item);
+
     return 0;
 }
