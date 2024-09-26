f53c903c0c36a5034ae5fa60a3cfcd74f1056248
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:46:58 +0530

    Delete cube_no.c

--- cube_no.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int n;
-    printf("enter number whose cube is required: \n");
-    scanf("%d",&n);
-    printf("cube of number %d is: %d ",n,n*n*n);
-    return 0;
-}
