3cb1ed54a1eff569a54dbbf87ac95c7ec0eccf4f
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:44:56 +0530

    Update shraddha_didi_hw.c

--- shraddha_didi_hw.c
+++ shraddha_didi_hw.c
@@ -70,3 +70,16 @@ int main(){
 }
 
 //area of rectangle
+#include <stdio.h>
+int main() {
+    //perimeter of rectangle
+    int a,b;
+    printf("enter length a of rectangle \n");
+    scanf("%d",&a);
+    printf("enter breadth b of rectangle \n");
+    scanf("%d",&b);
+    printf("area of rectangle is: %d",a*b);
+    return 0;
+}
+
+//smaller of teo numbers
