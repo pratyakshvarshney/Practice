e82ccdc1c9b0d36b6ab1a332074544a610802e54
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:44:10 +0530

    Update shraddha_didi_hw.c

--- shraddha_didi_hw.c
+++ shraddha_didi_hw.c
@@ -60,3 +60,13 @@ int main(){
 }
 
 //cube of a number
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter number whose cube is required: \n");
+    scanf("%d",&n);
+    printf("cube of number %d is: %d ",n,n*n*n);
+    return 0;
+}
+
+//area of rectangle
