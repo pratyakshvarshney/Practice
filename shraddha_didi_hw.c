f610214b6adb2d96effaeb7bd3408abb1be4e6d2
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-26 17:42:20 +0530

    Update shraddha_didi_hw.c

--- shraddha_didi_hw.c
+++ shraddha_didi_hw.c
@@ -44,3 +44,19 @@ int main(){
 }
 
 //check if input character is digit or not
+#include<stdio.h>
+int main(){
+    char chr;
+    int i;
+    printf("enter the character to be checked: \n");
+    scanf(" %c",&chr);
+        if(chr>='0' && chr<='9'){
+            printf("input character is digit \n");
+        }
+        else{
+            printf("input character is not digit \n");
+        }
+    return 0;
+}
+
+//cube of a number
