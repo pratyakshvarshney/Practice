2f3d6ef62898c8280ffcee745a1f49d87069a2a8
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-09-30 17:48:16 +0530

    Update ll_polynomial.c

--- ll_polynomial.c
+++ ll_polynomial.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+typedef struct polynode{
+    int coeff;
+    int exp;
+    struct polynode *next;
+}node;
+
+void print_ll(node *head){
+    node *temp=head;
+    printf("polynomial linked list is: \n");
+    while(temp){
+        printf("%d->")
+    }
+}
+
+node *createnode(int c,int e){
+    node *newnode=(node *)malloc(sizeof(node));
+    newnode->coeff=c;
+    newnode->exp=e;
+    newnode->next=NULL;
+    return newnode;
+}
+
+node *create_ll(node *head){
+    
+}
