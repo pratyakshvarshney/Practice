6c2708807d783e57f365efec5bf29e5a2fd4a465
Pratyaksh Varshney <pratyakshprakhar@gmail.com>
2024-12-18 15:43:48 +0530

    Update count_sort.c

--- count_sort.c
+++ count_sort.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 
 void countsort(int *arr, int n){
-    int k = arr[0];
+    int min_val = arr[0], max_val = arr[0];
     
-    for(int i=1; i<n; i++){
-        k = fmax(k, arr[i]);
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min_val) min_val = arr[i];
+        if (arr[i] > max_val) max_val = arr[i];
+    }
+
+    int offset = (min_val < 0) ? -min_val : 0;
+    int k = max_val + offset;
+    for (int i = 0; i < n; i++) {
+        arr[i] += offset;
     }
     
     int *count = (int *)calloc(k, sizeof(int));
@@ -19,16 +25,17 @@ void countsort(int *arr, int n){
         count[i] += count[i-1];
     }
     
-    int output[n];
+    int *output = (int *)malloc(n * sizeof(int));
     
     for(int i=n-1; i>=0; i--){
         output[--count[arr[i]]] = arr[i];
     }
     
     for(int i=0; i<n ;i++){
-        arr[i] = output[i];
+        arr[i] = output[i] - offset;
     }
-    
+
+    free(output);
     free(count);
     
 }
